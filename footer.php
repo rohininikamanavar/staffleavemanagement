0e4ca3d5e64987cdf3467fbc8d13b83073c2941c
rohininikamanavar <159284780+rohininikamanavar@users.noreply.github.com>
2024-11-06 12:13:37 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <div class="footer-area">
+                <p>© <?php echo date("Y");?> | JCER Leave Management System| Developed By <a href="#">ritesh.exe</a></p>
+        </div>
+</footer>
